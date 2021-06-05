807cc6248289f2c9e4a76159b2917971cbbdd2c7
Konstantin <konstantin@revimedia.com>
2021-06-05 16:48:30 +0300

    Apply logic to increase colors combination rathet than 1 color

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,7 +1,7 @@
 let isGameStarted = false;
 let level = 0;
 const buttonColors = ['green', 'red', 'yellow', 'blue'];
-let nextColor = '';
+const nextColors = [];
 
 $('body').keypress(e => { 
     console.log('Pressed: ', e.key, ', isGameStarted = ', isGameStarted);
@@ -18,9 +18,16 @@ $('div.btn').click(function () {
     }
     let buttonId = $(this).attr('id');
     playSound(buttonId);
-    addAnimationEffect(buttonId);
+    addAnimationEffect(buttonId, 100);
+    const nextColor = nextColors.shift();
+    console.log('onclick: = ', nextColors);
     if (buttonId === nextColor) {
-        setTimeout(() => { nextLevel()}, 100);
+        setTimeout(() => { 
+            if (nextColors.length ===0) {
+                nextLevel();
+            }           
+        }, 
+        100);
     } else {
         fail();
     }
@@ -28,11 +35,14 @@ $('div.btn').click(function () {
 
 function nextSequence() {
     console.log('nextSequence');
-    bthIndex = Math.floor(Math.random() * buttonColors.length);
-    nextColor = buttonColors[bthIndex];
-    console.log('nextColor = ', nextColor);
-    playSound(nextColor);
-    addAnimationEffect(nextColor);
+    for (let i=0; i<level; i++) {
+        const bthIndex = Math.floor(Math.random() * buttonColors.length);
+        nextColor = buttonColors[bthIndex];
+        nextColors.push(nextColor);
+        playSound(nextColor);
+        addAnimationEffect(nextColor, 200);
+    } 
+    console.log('nextSequence (init): ', nextColors);
 }
 
 function nextLevel() {
@@ -52,17 +62,18 @@ function fail() {
     }, 100);
     isGameStarted = false;
     level = 0;
+    nextColors.length = 0;
 }
 
 function playSound(buttonId) {
     new Audio(`sounds/${buttonId}.mp3`).play();
 }
 
-function addAnimationEffect(buttonId) {
+function addAnimationEffect(buttonId, timeout) {
     const activeButton = $(`#${buttonId}`);
     console.log(activeButton.html());
     activeButton.addClass('pressed');
     setTimeout(() => {
         activeButton.removeClass('pressed');
-    }, 100);
+    }, timeout);
 }
