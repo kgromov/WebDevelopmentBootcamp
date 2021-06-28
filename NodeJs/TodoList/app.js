cbc3f88cfcfd053026aad9e51fc230370e23e6b7
Konstantin <konstantin@revimedia.com>
2021-06-28 21:08:29 +0300

    Rewrite with native mongodbb for nodejs - really sucks

--- NodeJs/TodoList/app.js
+++ NodeJs/TodoList/app.js
@@ -1,56 +1,82 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const StringUtils = require("lodash");
-// const dateUtils = require(__dirname + "/date-utils.js");
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("resources"));
 
-mongoose.connect('mongodb://localhost:27017/todoList', {useNewUrlParser: true, useUnifiedTopology: true});
-
+// ---------- Define schema -------------------
 const defaultItemsText = ['Welcome to your todoList!', 
                       'Hit + to add new item', 
                       'Hit checkbox to delete item'
                     ];
+class Item {
+  constructor(name) {
+    this.name = name;
+  }
+}
 
-const itemSchema = new mongoose.Schema({ 
-  name: 'string' 
+class List {
+  constructor(name) {
+    this.name = name;
+    this.items = [];
+  }
+}
+
+const defaultItems = defaultItemsText.map(itemText => new Item(itemText));
+// console.log(defaultItems);
+// --------------------------------------------
+
+const client = new MongoClient('mongodb://localhost:27017/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const listSchema = new mongoose.Schema({ 
-  name: 'string', 
-  items: [itemSchema]
+client.connect(err => {
+  if (!err) {
+    console.log('Successfully connected to mongodb');
+  }
 });
 
-// Model - aka transient document object
-const Item = mongoose.model('Item', itemSchema);
-const List = mongoose.model('List', listSchema);
+const db = client.db('webDevBootcamp');
+const lists = db.collection('lists');
 
-
-const defaultItems = defaultItemsText.map(itemText => new Item({name: itemText}));
-
-// Item.insertMany(defaultItems, err => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log('Successfully insert items: ', defaultItems);
+// const defaultList = new List('default');
+// defaultList.items = [...defaultItems];
+// lists.insertOne(defaultList, err => {
+//   if (!err) {
+//     console.log('Inseted default list');
 //   }
-// });
+// }); 
+// client.close();
+
+/*
+ * Does not work properly:
+ * 1) TODO:to make it works - required to insert to items collection or generate id in constructor
+ * 2) deleteOne does not work for some reason (no errors);
+ * 3) Add list and item works
+*/
 
 app.get("/:listName", (req, res) => {
   const listName = StringUtils.capitalize(req.params.listName);
   
-  List.findOne({name: listName}, (err, list) => {
+  lists.findOne({name: listName}, (err, list) => {
     // TODO: handle error
     if (!list) {
-      const newList = new List({name: listName, items: defaultItems});
-      newList.save();
-      res.redirect(`/${listName}`);
+      const newList = new List(listName);
+      newList.items = [...defaultItems];
+      lists.insertOne(newList, err => {
+        if (!err) {
+          res.redirect(`/${listName}`);
+        }
+      });    
     } else {
+      // console.log('list = ', list);
       res.render("listItems", {listName: listName, items: list.items});
     }
   });  
@@ -62,12 +88,16 @@ app.post("/:listName", (req, res) => {
   const itemName = req.body.itemName; 
   console.log('POST: listName=', listName);
 
-   List.findOne({name: listName}, (err, list) => {
+  lists.findOne({name: listName}, (err, list) => {
     if (!err) {
-      const newItem = new Item({name: itemName});
-      list.items.push(newItem);
-      list.save();
-      res.redirect(`/${listName}`);
+      const newItem = new Item(itemName);
+      lists.updateOne({name: listName},
+        {$push: {items: newItem}},
+        err => {
+        if (!err) {
+          res.redirect(`/${listName}`);
+        }
+      });       
     } else {
       console.error(err);
     }
@@ -80,7 +110,7 @@ app.post("/delete/:listName/:itemId", (req, res) => {
   const itemId = StringUtils.capitalize(req.params.itemId);
   console.log(`DELETE: item by id=${itemId} from ${listName} list`);
 
-   List.findOneAndUpdate({name: listName}, 
+   lists.updateOne({name: listName}, 
     {$pull: {items: {_id: itemId}}}, 
     (err, list) => {
     if (!err) {
@@ -92,7 +122,7 @@ app.post("/delete/:listName/:itemId", (req, res) => {
 
 
 app.get("/", function(req, res) {
-  List.find({}, {name: 1}, (err, lists) => {
+  lists.find({}).toArray((err, lists) => {
     if (!err) {
       res.render("lists", {lists: lists});
     }    
@@ -106,11 +136,11 @@ app.post("/", function(req, res) {
 
 app.post("/delete/:listId", (req, res) => {
   const listId = StringUtils.capitalize(req.params.listId);
-  console.log(`DELETE: list by id=${listId}`);
+  console.log(`DELETE: list by _id=${listId}`);
 
-   List.findByIdAndDelete(listId, (err) => {
+   lists.deleteOne({_id: listId}, (err) => {
     if (!err) {
-      console.log(`Removed list by id=${listId}`);
+      console.log(`Removed list by _id=${listId}`);
       res.redirect("/");
     }        
   });   
